172f98f86c9cd220efc57fd1d2e79351548b1d66
Usama Hassan <ahmarkhan1233456@gmail.com>
2025-08-08 13:22:35 +0500

    set clerk express

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express'
 import "dotenv/config"
 import cors from 'cors'
 import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
